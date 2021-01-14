1c87819723e9aac9f98c5ba03d7bb0dd9ca27343
Nick Vaccaro <njvaccaro0@gmail.com>
2021-01-14 17:28:38 -0500

    Accurately highlight top nav items
    Nav item highlighting was pretty finnicky. There were 2 main issues:
     1. Selecting the brand nav item ("Nick Vaccaro") wouldn't highlight
        the 'about' nav item, even though that's where it took you.
     2. Navigating directly to a page without using the navbar (E.G. typing
        the URL directly) wouldn't highlight any nav item at all.
    
    These are both fixed now, largely by controlling the active nav item
    directly. I've also reduced duplicated code by putting all the nav
    items into an array and mapping over it. This is a bit cleaner, &
    hopefully it doesn't come back to bite me.

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function TopNav() {
+    const location = useLocation();
+
+    // Get the nav item that we should start with, on first page load.
+    // This is necessary because some pages, like the projects page, have additional data passed in
+    // that doesn't match up with the navItems list below.
+    const getStartingNavItem = () => {
+        // get rid of the starter /
+        let startNavItem = location.pathname.slice(1);
+
+        // If there's a second /, remove it and everything after it
+        let indexOfSlash = startNavItem.indexOf('/');
+        if(indexOfSlash > 0) {
+            startNavItem = startNavItem.substring(0, indexOfSlash);
+        }
+        return startNavItem;
+    }
+
+    const [activeNav, setActiveNav] = useState(getStartingNavItem());
+
+    const navItems = ['about', 'blog', 'projects', 'now'];
+    const navLink = (item) => {
+        return (
+            <Nav.Link 
+                as={Link} 
+                key={item}
+                eventKey={item}
+                to={`/${item}`} 
+                href={`/${item}`} 
+                className="nav-link"
+                onSelect={() => selectNav(item)}>
+                    {item.charAt(0).toUpperCase() + item.slice(1)}
+            </Nav.Link>
+        )
+    }
+
+    const selectNav = (item) => {
+        setActiveNav(item);
+    }
+
     return (
         <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
-            <Navbar.Brand as={Link} to="/about">Nick Vaccaro</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/about" href="/about" onClick={() => selectNav('about')}>Nick Vaccaro</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav>
-                    <Nav.Link as={Link} to="/about" href="/about" className="nav-link">About</Nav.Link>
-                    <Nav.Link as={Link} to="/blog" href="/blog" className="nav-link">Blog</Nav.Link>
-                    <Nav.Link as={Link} to="/projects" href="/projects" className="nav-link">Projects</Nav.Link>
-                    <Nav.Link as={Link} to="/now" href="/now" className="nav-link">Now</Nav.Link>
+                <Nav activeKey={activeNav}>
+                    {navItems.map(item => navLink(item))}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
